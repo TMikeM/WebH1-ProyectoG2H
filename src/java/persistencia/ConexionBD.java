3b145d94f95978da3e1c38fb243928ed158740a0
Lizeth <Mike primer commit>
2022-08-07 18:21:51 -0500

    Creacion de archivo peticiones de proveedor, falta comenzar con la interfaz

--- src/java/persistencia/ConexionBD.java
+++ src/java/persistencia/ConexionBD.java
@@ -25,7 +25,7 @@ public class ConexionBD {
         db = "zapateria";
         url = "jdbc:mysql://" + host + "/" + db+"?serverTimezone=UTC"; 		//URL DB
         username = "root";                      			//usuario base de datos global 
-        password = "17012002";
+        password = "1234";
         try {
             //Asignacin del Driver
             Class.forName(DB_driver);
